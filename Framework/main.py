2708e37a9af047445ed8ab2c6b5a5de995adf16a
Ryan Park <ryandpark2006@gmail.com>
2022-02-23 15:51:27 -0500

    simplified

---
+++ Framework/main.py
@@ -0,0 +1,20 @@
+# move right: left wheels move a certain number of revolutions 
+# move left: right wheels move a certain number of revolutions 
+# move forward: all wheels move forward
+# move backward: all wheels move backward 
+
+
+
+
+# framework:
+# once altimeter reaches specific height: move 2 feet ahead (whatever requirement is)
+# standby for voice listening 
+# IF keyword is detected:
+# execute keyword detected and also take in number which represent distance in feet
+# once executed standby
+#  
+
+
+command = "forward"
+
+if(command.__contains__("forward")):
--- main.py
+++
@@ -1,2 +0,0 @@
-# using if statements, define what will happen with robot
-# e.g. if forward is detected write code for that
